2fb9d6d8fce14b3c48a8795dcb213419d0739b58
Jim Chiang <jchiang@slac.stanford.edu>
2016-03-24 23:02:42 -0700

    modify create table statement

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -3,7 +3,7 @@ import subprocess
 
 connection = MySQLdb.connect(db='myapp_test', user='travis')
 cursor = connection.cursor()
-cursor.execute('create table ForcedSource if not exists (objectId BIGINT);')
+cursor.execute('create table ForcedSource (objectId BIGINT);')
 cursor.execute('insert into ForcedSource values (1),(2),(3),(4),(5);')
 cursor.close()
 connection.commit()
