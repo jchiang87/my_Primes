a5c360b14c3718c2bded73af835c3540d661bdd7
Jim Chiang <jchiang@slac.stanford.edu>
2016-03-25 21:32:55 -0700

    test mysql usage in travis ci

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -1,11 +1,61 @@
+import unittest
 import MySQLdb
-import subprocess
 
-connection = MySQLdb.connect(db='myapp_test', user='travis')
-cursor = connection.cursor()
-cursor.execute('create table ForcedSource (objectId BIGINT);')
-cursor.execute('insert into ForcedSource values (1),(2),(3),(4),(5);')
-cursor.close()
-connection.commit()
+def get_db_info():
+    """
+    Try to connect to Travis CI MySQL services or the user's via
+    ~/.my.cnf and return the connection info.  Otherwise, return
+    an empty dict, which should skip the tests.
+    """
+    db_info = {}
+    try:
+        try:
+            # Travis CI usage:
+            my_db_info = dict(db='myapp_test', user='travis')
+            test = MySQLdb.connect(**my_db_info)
+        except Exception, eobj:
+            print eobj
+            # User's configuration:
+            my_db_info = dict(read_default_file='~/.my.cnf')
+            test = MySQLdb.connect(**my_db_info)
+        test.close()
+        db_info = my_db_info
+    except Exception, eobj:
+        print eobj
+        pass
+    return db_info
 
-subprocess.call("mysql -e 'select * from myapp_test.ForcedSource'", shell=True)
+_db_info = get_db_info()
+
+@unittest.skipUnless(_db_info, "MySQL database not available")
+class MySQL_TestCase(unittest.TestCase):
+    "Class to test MySQL database access in Travis CI."
+
+    def setUp(self):
+        "Create a database connection."
+        global _db_info
+        self.connection = MySQLdb.connect(**_db_info)
+        self.table_name = 'test_table'
+
+    def tearDown(self):
+        "Drop the test table and close the connection."
+        cursor = self.connection.cursor()
+        cursor.execute('drop table if exists %s' % self.table_name)
+        cursor.close()
+        self.connection.commit()
+        self.connection.close()
+
+    def test_create_insert_and_select(self):
+        "Test table creation, and row insertion and selection."
+        cursor = self.connection.cursor()
+        cursor.execute('create table %s (objectId BIGINT)' % self.table_name)
+        cursor.execute('insert into %s values (1),(2),(3),(4),(5)'
+                       % self.table_name)
+        cursor.close()
+        self.connection.commit()
+        cursor = self.connection.cursor()
+        cursor.execute('select * from %s order by objectId asc'
+                       % self.table_name)
+        results = [x[0] for x in cursor]
+        cursor.close()
+        self.assertEqual(results, range(1, 6))
